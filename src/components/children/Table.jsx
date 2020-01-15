e97103e0c24cee56ef44537917f57590f7d3dde5
Chen Wang <cwang138@illinois.edu>
2020-01-15 15:41:11 -0600

    upgrade handsontable

--- src/components/children/Table.jsx
+++ src/components/children/Table.jsx
@@ -1,47 +1,51 @@
 import React, { Component } from "react";
-import Handsontable from "handsontable";
-import "handsontable/dist/handsontable.css";
+// import Handsontable from "handsontable";
+// import "handsontable/dist/handsontable.css";
+import { HotTable } from "@handsontable/react";
 
 class Table extends Component {
 
 	constructor(props) {
 		super(props);
+		this.state = {
+			settings: {}
+		}
 	}
 
 	componentDidMount() {
-		this.datagrid = new Handsontable(
-			document.getElementById(this.props.container), {
+		this.this.setState({
+			settings:{
 				data: this.props.data,
 				rowHeaders: this.props.rowHeaders,
 				colHeaders: this.props.colHeaders,
 				observeChanges: true,
 				height: this.props.height,
 				overflow: "auto",
-				stretchH: "all"
-			});
+				stretchH: "all"			}
+
+		});
 	}
 
-	componentWillUpdate() {
-		this.datagrid.destroy();
-		this.datagrid = new Handsontable(
-			document.getElementById(this.props.container), {
-				data: this.props.data,
-				rowHeaders: this.props.rowHeaders,
-				colHeaders: this.props.colHeaders,
-				observeChanges: true,
-				height: this.props.height,
-				overflow: "auto",
-				stretchH: "all"
-			});
-	}
+	// componentWillUpdate() {
+	// 	this.datagrid.destroy();
+	// 	this.datagrid = new Handsontable(
+	// 		document.getElementById(this.props.container), {
+	// 			data: this.props.data,
+	// 			rowHeaders: this.props.rowHeaders,c
+	// 			colHeaders: this.props.colHeaders,
+	// 			observeChanges: true,
+	// 			height: this.props.height,
+	// 			overflow: "auto",
+	// 			stretchH: "all"
+	// 		});
+	// }
 
-	componentWillUnmount() {
-		this.datagrid.destroy();
-	}
+	// componentWillUnmount() {
+	// 	this.datagrid.destroy();
+	// }
 
 	render() {
-		return <div id={this.props.container} />;
-
+		return <HotTable settings={this.state.settings} id={this.props.container} />;
 	}
 }
 
