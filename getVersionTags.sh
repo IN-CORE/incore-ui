2bc2ce288e25ec8022e0bbf7f0a276028dff2cb8
Chen Wang <cwang138@illinois.edu>
2020-07-07 12:48:22 -0500

    fix typo

--- getVersionTags.sh
+++ getVersionTags.sh
@@ -6,10 +6,10 @@ pyincoreViz=$(curl -s 'https://api.github.com/repos/IN-CORE/pyincore-viz/tags' |
 incoreUI=$(curl -s 'https://api.github.com/repos/IN-CORE/incore-ui/tags' | jq '.[0].name' || echo '"NA"')
 incoreServices=$(curl -s 'https://api.github.com/repos/IN-CORE/incore-services/tags' | jq '.[0].name' || echo '"NA"')
 incoreLab=$(curl -s 'https://api.github.com/repos/IN-CORE/incore-lab/tags' | jq '.[0].name' || echo '"NA"')
-inocreDocs=$(curl -s 'https://api.github.com/repos/IN-CORE/incore-docs/tags' | jq '.[0].name' || echo '"NA"')
+incoreDocs=$(curl -s 'https://api.github.com/repos/IN-CORE/incore-docs/tags' | jq '.[0].name' || echo '"NA"')
 
 echo {\"pyincore\":$pyincore, \"pyincore-viz\":$pyincoreViz, \"incore-ui\":$incoreUI, \"incore-services\":$incoreServices, \
-\"incore-lab\":$incoreLab, \"incore-docs\":$inocreDocs} > github.json
+\"incore-lab\":$incoreLab, \"incore-docs\":$incoreDocs} > github.json
 
 
 
