005d1acd95f1f092573f747ca5acc16fe8d5bc66
Chen Wang <cwang138@illinois.edu>
2021-10-19 18:17:36 -0500

    3d

--- src/app.config.js
+++ src/app.config.js
@@ -19,7 +19,9 @@ config["dataServiceBase"] = `${hostname}/data/api/`;
 config["dataService"] = `${hostname}/data/api/datasets`;
 config["incoreLab"] = `${hostname}/hub/`;
 config["geoServer"] = `${hostname}/geoserver/incore/wms`;
-config["plottingService"] = `${hostname}/plotting/api/samples`;
+// config["plottingService"] = `${hostname}/plotting/api/samples`;
+// TODO remove when deployed
+config["plottingService"] = "http://localhost:5000/plotting/api/samples";
 
 config["testUserInfo"] = "incrtest";
 config["hazardDatasetTypes"] = [
