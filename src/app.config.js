562f0980b357507f490581ece5309dbcb2aaa67b
Rashmil Panchani <damonsalvatore833@gmail.com>
2023-02-09 10:48:56 -0600

    fix typo in slc url

--- src/app.config.js
+++ src/app.config.js
@@ -45,7 +45,7 @@ config["maxUsage"] = {
 };
 config["playbookImageDetails"] = [
 	{ app: "Galveston", img: "/public/galveston.jpeg", url: "/playbook/galveston/" },
-	{ app: "Salt Lake City", img: "/public/salt-lake-city.jpeg", url: "/plabyook/slc/" },
+	{ app: "Salt Lake City", img: "/public/salt-lake-city.jpeg", url: "/playbook/slc/" },
 	{ app: "Joplin", img: "/public/joplin-main-street.jpeg", url: "/playbook/joplin/" }
 ];
 
