87f9ca0fb3eda3987541c5040d9ece8233a69bc3
Chen Wang <cwang138@illinois.edu>
2023-11-02 11:21:01 -0500

    center everything

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -69,6 +69,7 @@ const styles = (theme) => ({
 		margin: "2em auto",
 		borderRadius: "0.5em",
 		background: "#eeeeee",
+		textAlign: "center"
 	},
 	versionSection: {
 		margin: theme.spacing(0.5)
